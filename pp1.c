e500486a5919d6a1a710e23a435c010bee9899ab
Periyasamy10 <35652731+Periyasamy10@users.noreply.github.com>
2018-01-30 15:47:57 +0530

    Create pp1.c

---
+++ pp1.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+void main()
+{
+    int n,m,i;
+    printf("enter the value of n");
+    scanf("%d",&n);
+    for(i=0;i<=n;i++)
+    {
+        m+=i;
+    }
+    printf("%d",m);
+}
