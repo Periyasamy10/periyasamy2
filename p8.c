bcd742246b42b7499ec392bff15f93dc15437206
Periyasamy10 <35652731+Periyasamy10@users.noreply.github.com>
2018-02-20 15:45:05 +0530

    Create p8.c

---
+++ p8.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+{
+int n,a,d,s,ans=0,i;
+scanf("%d",&n);
+scanf("%d",&a);
+scanf("%d",&d);
+for(i=1;i<=n;i++)
+{
+s=a+(i-1)*d;
+ans=ans+s;
+}
+printf("%d",ans);
+return 0;
+}
